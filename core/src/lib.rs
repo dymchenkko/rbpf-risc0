2afbdde9849a7f9ca329ac99d1e5761788a757f1
Carsten Munk <carsten@zippie.com>
2022-11-07 16:34:13 +0100

    Make failure case smaller
    Signed-off-by: Carsten Munk <carsten@zippie.com>

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,39 +1,75 @@
-use rbpf::ebpf;
+use byteorder::{ByteOrder, LittleEndian};
 use std::io::{Error, ErrorKind};
+
+
+pub const INSN_SIZE: usize = 8;
+pub const BPF_JMP   : u8 = 0x05;
+pub const BPF_EXIT  : u8 = 0x90;
+pub const EXIT       : u8 = BPF_JMP   | BPF_EXIT;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Insn {
+    /// Operation code.
+    pub opc: u8,
+    /// Destination register operand.
+    pub dst: u8,
+    /// Source register operand.
+    pub src: u8,
+    /// Offset operand.
+    pub off: i16,
+    /// Immediate value operand.
+    pub imm: i32,
+}
+
+pub fn get_insn(prog: &[u8], idx: usize) -> Insn {
+    // This guard should not be needed in most cases, since the verifier already checks the program
+    // size, and indexes should be fine in the interpreter/JIT. But this function is publicly
+    // available and user can call it with any `idx`, so we have to check anyway.
+    if (idx + 1) * INSN_SIZE > prog.len() {
+        panic!("Error: cannot reach instruction at index {:?} in program containing {:?} bytes",
+               idx, prog.len());
+    }
+    Insn {
+        opc:  prog[INSN_SIZE * idx],
+        dst:  prog[INSN_SIZE * idx + 1] & 0x0f,
+        src: (prog[INSN_SIZE * idx + 1] & 0xf0) >> 4,
+        off: LittleEndian::read_i16(&prog[(INSN_SIZE * idx + 2) .. ]),
+        imm: LittleEndian::read_i32(&prog[(INSN_SIZE * idx + 4) .. ]),
+    }
+}
+
 
 pub fn test_execute_program2(prog: &[u8], mut reg: [u64;11]) -> Result<u64, Error> {
 
     let mut insn_ptr:usize = 0;
 
-    while insn_ptr * ebpf::INSN_SIZE < prog.len() {
-        let insn = ebpf::get_insn(prog, insn_ptr);
+    while insn_ptr * INSN_SIZE < prog.len() {
+        let insn = get_insn(prog, insn_ptr);
         insn_ptr += 1;
         let _dst = insn.dst as usize;
         eprintln!("{:?}", insn);
         eprintln!("{:?}", reg);
-        if (insn.opc == ebpf::EXIT) { return Ok(reg[0]) }
+        if (insn.opc == EXIT) { return Ok(reg[0]) }
         reg[_dst] = insn.imm  as u32                                as u64;
     }
     unreachable!()
 }
 
 
-pub fn test_execute_program(s: rbpf::EbpfVmNoData, mut prog: &[u8]) -> Result<u64, Error> {
+pub fn test_execute_program(mut prog: &[u8]) -> Result<u64, Error> {
         
-    let stack = vec![0u8;ebpf::STACK_SIZE];
-
     let mut reg: [u64;11] = [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, stack.as_ptr() as u64 + stack.len() as u64
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 //stack.as_ptr() as u64 + stack.len() as u64
     ];
 
     let mut insn_ptr:usize = 0;
-    while insn_ptr * ebpf::INSN_SIZE < prog.len() {
-        let insn = ebpf::get_insn(prog, insn_ptr);
+    while insn_ptr * INSN_SIZE < prog.len() {
+        let insn = get_insn(prog, insn_ptr);
         insn_ptr += 1;
         let _dst = insn.dst as usize;
         eprintln!("{:?}", insn);
         eprintln!("{:?}", reg);
-        if (insn.opc == ebpf::EXIT) { return Ok(reg[0]) }
+        if (insn.opc == EXIT) { return Ok(reg[0]) }
         reg[_dst] = insn.imm  as u32                                as u64;
     }
     unreachable!()
--- methods/guest/src/bin/method_name.rs
+++ methods/guest/src/bin/method_name.rs
@@ -3,12 +3,10 @@
 #![no_main]
 
 risc0_zkvm_guest::entry!(main);
-use rbpf::ebpf;
+
 use execute_test;
 
 pub fn main(){
-    // TODO: Implement your guest code here
-
     let prog = &[
         0xb4, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // mov32 r0, 0
         0xb4, 0x01, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, // mov32 r1, 2
@@ -17,23 +15,16 @@ pub fn main(){
         0x95, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00  // exit
     ];
 
-    let stack = vec![0u8;ebpf::STACK_SIZE];
-
-        let mut reg: [u64;11] = [
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, stack.as_ptr() as u64 + stack.len() as u64
-        ];
+    let mut reg: [u64;11] = [
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
+    ];
 
 
-    // Instantiate a struct EbpfVmNoData. This is an eBPF VM for programs that
-    // takes no packet data in argument.
-    // The eBPF program is passed to the constructor.
-    let vm = rbpf::EbpfVmNoData::new(Some(prog)).unwrap();
     eprintln!("Initializing reg inside the function:");
 
-    let result = execute_test::test_execute_program(vm, prog);
+    let result = execute_test::test_execute_program(prog);
 
     eprintln!("Reg as input:");
     let result = execute_test::test_execute_program2(prog, reg);
 
-    // Execute (interpret) the program. No argument required for this VM.
 }
